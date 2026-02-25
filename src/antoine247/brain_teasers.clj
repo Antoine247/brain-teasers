8d5b85f1daad26da1e12add4e31159b65c6bf4e9
Antoine247 <chamoun863@gmail.com>
2026-02-25 13:52:53 -0300

    puzzles 1,2,3

--- src/antoine247/brain_teasers.clj
+++ src/antoine247/brain_teasers.clj
@@ -1,12 +1,40 @@
 (ns antoine247.brain-teasers
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+;; equality
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(= 1 1.0 1N 1.0M)
+;;=>false
+(== 1 1.0 1N 1.0M)
+;;=>true
+
+;; mas comun usar el =, el == es utilo para casos de operaciones con distintos tipos de numeros
+
+;; sumas
+
+(def bignum 9223372036854775807)
+(def biggernum 9223372036854775808)
+
+(= biggernum (+ bignum 1))
+;;=>exception overflow
+(= biggernum (+' bignum 1))
+;;=>true
+(= biggernum (unchecked-add bignum 1))
+;;=>false
+
+;; suma normal (+) chequea overflows, si te llegas a pasar de bytes para un tipo de numero, va a lanzar arithmeticExcepcion
+;; suma "promotora" (+') en el caso de que un numero exceda los bytes de un tipo, lo promueve a un tipo de numero con mayor
+;; capacidad, por ejemplo de long a BigInt.
+;; suma sin proteccion (unchecked-add) no chequea overflows, si te pasas de bytes en un long por ejemplo, da la vuelta
+;; y se hace un numero negativo.
+
+;; ##NaNs
+
+(= ##NaN ##NaN)
+;;=> false
+(== ##NaN ##NaN)
+;;=> false
+
+;; un NaN "Not a Number" es una expresion numerica de java para representar un numero que no es un numero, que no se puede calcular
+(/ 0.0 0.0)
+;;=>##NaN
