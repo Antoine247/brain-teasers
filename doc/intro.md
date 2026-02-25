d125707c7341171471ec5792da975194ba77961b
Antoine247 <chamoun863@gmail.com>
2026-02-25 13:30:45 -0300

    creacion projecto

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to antoine247/brain-teasers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/antoine247/brain_teasers_test.clj
@@ -0,0 +1,7 @@
+(ns antoine247.brain-teasers-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [antoine247.brain-teasers :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
